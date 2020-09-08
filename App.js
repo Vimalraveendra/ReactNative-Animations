ea8004575b7480b660c20306be788aeb665ddee2
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-08 12:31:16 +0200

    Updated App component to render fadeIn and fadeout animations

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Animated,
+  TouchableOpacity,
+} from 'react-native';
 
 class App extends React.Component {
+  state = {
+    fadeAnimation: new Animated.Value(0),
+  };
+
+  fadeIn = () => {
+    Animated.timing(this.state.fadeAnimation, {
+      toValue: 1,
+      duration: 5000,
+      useNativeDriver: true,
+    }).start(() => {
+      this.fadeOut();
+    });
+  };
+
+  fadeOut = () => {
+    Animated.timing(this.state.fadeAnimation, {
+      toValue: 0,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  };
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Animations</Text>
+        <Animated.View
+          style={[styles.animatedView, {opacity: this.state.fadeAnimation}]}
+        />
+        <TouchableOpacity style={styles.btn} onPress={this.fadeIn}>
+          <Text style={styles.btnText}>Animations</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     );
   }
@@ -18,9 +50,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  title: {
-    fontSize: 23,
-    fontWeight: '700',
+  animatedView: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+  btn: {
+    backgroundColor: 'skyblue',
+    height: 45,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '500',
+    paddingHorizontal: 15,
+    padding: 12,
   },
 });
 
